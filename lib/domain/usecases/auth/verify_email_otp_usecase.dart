7deca482ce9a7e89002146e4fdfa68ab90684a20
CoupDerace <turtusibae@gmail.com>
2026-06-13 20:43:24 +0700

    feat(auth): add email OTP verification use case
    cakupan: use case verifikasi OTP email user

---
+++ lib/domain/usecases/auth/verify_email_otp_usecase.dart
@@ -0,0 +1,8 @@
+
+
+class VerifyEmailOtpUsecase {
+  final AuthRepository _repository;
+  VerifyEmailOtpUsecase(this._repository);
+
+  Future<void> call(String code) => _repository.verifyEmailOtp(code);
+}
