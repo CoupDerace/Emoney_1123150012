4296160baf2a5453752fda5829b98dd8e99958ba
CoupDerace <turtusibae@gmail.com>
2026-06-13 14:07:21 +0700

    feat(otp): add OTP remote datasource contract
    cakupan: abstraksi datasource remote untuk OTP dan TOTP authentication

---
+++ lib/data/datasources/remote/otp_remote_datasource.dart
@@ -0,0 +1,9 @@
+
+
+abstract class OtpRemoteDatasource {
+  Future<OtpSentEntity> sendOtpFirebase();
+  Future<OtpSentEntity> sendOtpEmail();
+  Future<void> confirmOtp({required String code, required String otpType});
+  Future<TotpSetupEntity> registerTotp();
+  Future<bool> verifyTotp(String code);
+}
