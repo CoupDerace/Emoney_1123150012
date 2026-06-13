e9796e416d95254eaa697e15bf3bb5a645abb2f8
CoupDerace <turtusibae@gmail.com>
2026-06-13 11:53:43 +0700

    feat(auth): add auth remote datasource contract
    cakupan: abstraksi datasource remote untuk autentikasi dan user session

---
+++ lib/data/datasources/remote/auth_remote_datasource.dart
@@ -0,0 +1,10 @@
+
+
+abstract class AuthRemoteDatasource {
+  Future<({UserModel user, String token})> verifyFirebaseToken(String firebaseToken);
+  Future<({UserModel user, String token})> registerWithOtp(String firebaseToken);
+  Future<void> verifyEmailOtp(String code);
+  Future<UserModel> getMe();
+  Future<void> updateFcmToken(String fcmToken);
+  void clearAuthToken();
+}
