38d5fb6843042cc6b3a177d0ff59370d169059d5
CoupDerace <turtusibae@gmail.com>
2026-06-13 17:42:47 +0700

    feat(otp): add repository dependency injection
    cakupan: inject OtpRemoteDatasource dan AuthRepository ke repository

--- lib/data/repositories/otp_repository_impl.dart
+++ lib/data/repositories/otp_repository_impl.dart
@@ -1,3 +1,7 @@
 class OtpRepositoryImpl implements OtpRepository {
+  final OtpRemoteDatasource _remote;
+  final AuthRepository _authRepo;
+  OtpRepositoryImpl(this._remote, this._authRepo);
+
   
 }
