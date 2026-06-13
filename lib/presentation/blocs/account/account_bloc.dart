935936321263723e66acc2005d4faf4d1cc762bb
CoupDerace <turtusibae@gmail.com>
2026-06-13 22:16:58 +0700

    feat(account): add account bloc state definitions
    cakupan: definisi state initial, loading, loaded, dan error untuk account bloc

--- lib/presentation/blocs/account/account_bloc.dart
+++ lib/presentation/blocs/account/account_bloc.dart
@@ -8,3 +8,22 @@ abstract class AccountEvent extends Equatable {
 class AccountLoadRequested extends AccountEvent {}
 class AccountRefreshRequested extends AccountEvent {}
 
+abstract class AccountState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+class AccountInitial extends AccountState {}
+class AccountLoading extends AccountState {}
+class AccountLoaded extends AccountState {
+  final AccountEntity account;
+  final List<TransactionEntity> transactions;
+  AccountLoaded({required this.account, required this.transactions});
+  @override
+  List<Object?> get props => [account, transactions];
+}
+class AccountError extends AccountState {
+  final String message;
+  AccountError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
