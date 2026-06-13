e496f657b217e6438b0e8a81cd2e48df5a5c9417
CoupDerace <turtusibae@gmail.com>
2026-06-13 23:23:54 +0700

    feat(otp): add OTP bloc state definitions
    cakupan: definisi seluruh state OTP dan TOTP flow

--- lib/presentation/blocs/auth/0tp_bloc.dart
+++ lib/presentation/blocs/auth/0tp_bloc.dart
@@ -23,3 +23,38 @@ class OtpVerifyTotp extends OtpEvent {
   List<Object?> get props => [code];
 }
 class OtpReset extends OtpEvent {}
+
+// States
+abstract class OtpState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class OtpInitial extends OtpState {}
+class OtpLoading extends OtpState {}
+class OtpSent extends OtpState {
+  final OtpSentEntity entity;
+  OtpSent(this.entity);
+  @override
+  List<Object?> get props => [entity];
+}
+class OtpVerified extends OtpState {}
+class OtpTotpSetup extends OtpState {
+  final TotpSetupEntity entity;
+  OtpTotpSetup(this.entity);
+  @override
+  List<Object?> get props => [entity];
+}
+class OtpTotpEnabled extends OtpState {}
+class OtpInvalid extends OtpState {
+  final String message;
+  OtpInvalid(this.message);
+  @override
+  List<Object?> get props => [message];
+}
+class OtpError extends OtpState {
+  final String message;
+  OtpError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
