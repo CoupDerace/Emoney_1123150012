c249030c321d186d45bfb655a8ac7fc5b0fd39a4
CoupDerace <turtusibae@gmail.com>
2026-06-13 20:09:49 +0700

    feat(auth): add email OTP verification contract
    cakupan: kontrak verifikasi OTP email

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,7 +1,7 @@
 abstract class AuthRepository {
   Future<({UserEntity user, String token})> verifyFirebaseToken(String firebaseToken);
   Future<({UserEntity user, String token})> registerWithOtp(String firebaseToken);
-
+  Future<void> verifyEmailOtp(String code);
 
 
 }
